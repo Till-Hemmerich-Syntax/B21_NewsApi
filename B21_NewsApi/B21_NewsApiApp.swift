bbfd119fe3b5cbb443e68e336db96d4eff456559
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2024-11-22 09:04:01 +0100

    Initial Commit

---
+++ B21_NewsApi/B21_NewsApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  B21_NewsApiApp.swift
+//  B21_NewsApi
+//
+//  Created by Till Hemmerich on 22.11.24.
+//
+
+import SwiftUI
+
+@main
+struct B21_NewsApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ B21_NewsApiTests/B21_NewsApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  B21_NewsApiTests.swift
+//  B21_NewsApiTests
+//
+//  Created by Till Hemmerich on 22.11.24.
+//
+
+import Testing
+@testable import B21_NewsApi
+
+struct B21_NewsApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
